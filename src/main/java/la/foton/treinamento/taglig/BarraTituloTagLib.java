44c712a09d810430dccec349360800af31ff10df
mcqueide <mcqueide@gmail.com>
2015-03-06 10:19:15 -0300

    asdflkjasf

--- src/main/java/la/foton/treinamento/taglig/BarraTituloTagLib.java
+++ src/main/java/la/foton/treinamento/taglig/BarraTituloTagLib.java
@@ -1,5 +1,8 @@
 package la.foton.treinamento.taglig;
 
+import java.io.IOException;
+import java.text.MessageFormat;
+
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.TagSupport;
 
@@ -9,8 +12,19 @@ public class BarraTituloTagLib extends TagSupport {
 
 	private String valor;
 	
+	private static final String HTML
+	= "<div style=\"padding: 10px; color: #fff; background: #000;\">" +
+	"<h1>{0}</h1>" +
+	"</div>";
+	
 	@Override
 	public int doStartTag() throws JspException {
+		String valorCompleto = MessageFormat.format(HTML, valor);
+		try {
+			pageContext.getOut().write(valorCompleto);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return super.doStartTag();
 	}
 	
--- src/main/webapp/cadastrocliente/cadastro.jsp
+++ src/main/webapp/cadastrocliente/cadastro.jsp
@@ -1,5 +1,6 @@
 <%@page import="java.util.Date"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://www.foton.la/treinamento.tld" prefix="treinamento" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -9,6 +10,8 @@
 <title>Cadastro de Cliente</title>
 </head>
 <body>
+
+	<treinamento:titulo valor="Cadastro de cliente"></treinamento:titulo>
 
 	<%
 		Date dataAtual = (Date) request.getAttribute("dataAtual");
